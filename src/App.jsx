10f5ce4958fe65a52eb10f182f2963a4a87464ff
oli <olicodes@zohomail.com>
2025-11-30 16:38:06 +0330

    notebook flipping gsap

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,55 @@
+import gsap from "gsap";
+import { dataset } from "./assets/data/data"
+
 function App() {
 
+    let firstDrag = 0;
+
+    const handleDragStart = (e) => {
+      firstDrag = e.clientX;
+    }
+
+    const flipAnimation = (element, rotateY) => {
+      const isFirstLast = (element.style.zIndex == 1 || element.style.zIndex == dataset.length)
+      element.classList.toggle("dragged")
+      gsap.to(element, {
+        transformOrigin: 'center left',
+        rotateY,
+        duration: isFirstLast ? 3 : 0.3,
+        ease: isFirstLast ? "elastic" : "sine.inOut"
+      })
+      element.style.zIndex = dataset.length - element.style.zIndex + 1;
+    }
+
+    const handleDragEnd = (e) => {
+        if(e.clientX < firstDrag) {
+          flipAnimation(e.target, 180)
+        }
+        else {
+          flipAnimation(e.target, 0)
+        }
+    }
+
+
     return (
-      <>
-      </>
+      <div className="h-[100vh] w-full flex justify-center items-center">
+        <div className="bookContainer w-[150px] h-[150px] lg:w-[300px] lg:h-[300px]">
+          {
+            dataset.map(item => {
+              return (
+                <div 
+                  key={item.zIndex}
+                  className="w-[150px] h-[150px] lg:w-[300px] lg:h-[300px] imgRatio"
+                  style={{backgroundImage: `url(${item.pic})`, zIndex: item.zIndex}}
+                  onDragEnd={handleDragEnd}
+                  onDragStart={handleDragStart}
+                >
+                </div>
+              )
+            })
+          }
+        </div>
+      </div>
     )
 }
 
---
+++ src/assets/data/data.js
@@ -0,0 +1,15 @@
+import ikigai from '../pics/ikigai.jpg';
+import ikebana from '../pics/ikebana.jpg';
+import kaizen from '../pics/kaizen.jpg';
+import shinrinyoko from '../pics/shinrinyoko.jpg';
+import ukiyo from '../pics/ukiyo.jpg';
+import wabi from '../pics/wabi.jpg';
+
+export const dataset = [
+    { zIndex: 1, pic: ikigai },
+    { zIndex: 2, pic: ikebana },
+    { zIndex: 3, pic: kaizen },
+    { zIndex: 4, pic: shinrinyoko },
+    { zIndex: 5, pic: ukiyo },
+    { zIndex: 6, pic: wabi },
+]
